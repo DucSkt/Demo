0210fda8c4d1af7cb351c91f05e00700c3607ff2
DucSkt <ducskt11@gmail.com>
2019-01-06 15:26:57 +0700

    Doi vsvs

--- AWS/App.js
+++ AWS/App.js
@@ -42,7 +42,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     backgroundColor: '#F5FCFF',
   },
   welcome: {
